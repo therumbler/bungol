0fcdf41bcc22ee5cab20667b3aff05692f1d19bf
Benjamin Rumble <therumbler@gmail.com>
2019-06-01 10:52:15 -0400

    using BungolProperty

--- bungol/bungol.py
+++ bungol/bungol.py
@@ -2,8 +2,11 @@
 import json
 import logging
 import re
+from typing import Optional
 
 import aiohttp
+
+from .models import BungolProperty
 
 logger = logging.getLogger(__name__)
 
@@ -31,20 +34,23 @@ class Bungol:
             return await resp.json()
 
     @staticmethod
-    def _html_to_property(html: str) -> dict:
+    def _html_to_property(html: str) -> Optional[BungolProperty]:
         pattern = r"let property = (.*\}?);"
         match = re.search(pattern, html)
         if not match:
             logger.error("no match")
-            return {}
+            return None
         json_str = match.group(1)
-        return json.loads(json_str)
+        obj = json.loads(json_str)
+        return BungolProperty(**obj)
 
-    async def get_property(self, slug: str, mls_number: str, listing_id: int) -> dict:
+    async def get_property(
+        self, mls_number: str, listing_id: int
+    ) -> Optional[BungolProperty]:
         """get the property dict"""
-        full_slug = f"{slug}-{mls_number}-{listing_id}"
+        full_slug = f"PLACEHOLDER-{mls_number}-{listing_id}"
         url = f"https://www.bungol.ca/listing/{full_slug}"
         async with self.session.get(url) as resp:
             html = await resp.text()
-            property = self._html_to_property(html)
-        return property
+            property_ = self._html_to_property(html)
+        return property_
