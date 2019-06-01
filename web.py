a9bbfb39fee6fa3b7bab54df759591717d25188d
Benjamin Rumble <therumbler@gmail.com>
2019-06-01 08:54:59 -0700

    template update

--- web.py
+++ web.py
@@ -1,8 +1,14 @@
+#!/home/irumble/.local/share/virtualenvs/bungol-TZ6KT7hd/bin/python
+from argparse import ArgumentParser
+import os
+
 from aiohttp import web
 
 from bungol import Bungol, BungolProperty
 
-with open("./bungol/templates/index.html") as f:
+TEMPLATES_PATH = os.getenv('BUNGOL_TEMPLATES_PATH')
+
+with open(f"{TEMPLATES_PATH}index.html") as f:
     HTML_TEMPLATE = f.read()
 
 
@@ -16,14 +22,24 @@ async def index(request):
     if not resp:
         return web.HTTPNotFound(text="propery not found")
     content = resp.to_html()
-    html = HTML_TEMPLATE.format(content=content)
+    html = HTML_TEMPLATE.format(content=content, title=resp.info["street_name"])
     return web.Response(content_type="text/html", text=html)
 
 
-def main():
+def make_app():
     app = web.Application()
     app.add_routes([web.get("/{mls_number}-{listing_id}", index)])
-    web.run_app(app)
+    return app
+
+def main():
+    parser = ArgumentParser(description='Bungol web app')
+    parser.add_argument('--port', help='the HTTP port')
+    args = parser.parse_args()
+    port = args.port if args.port else 2021
+    app = make_app()
+
+        
+    web.run_app(app, port=port)
 
 
 if __name__ == "__main__":
