04a3f8d7aa513234d9b93c6731cfb72d6bf9dc05
Benjamin Rumble <therumbler@gmail.com>
2019-06-01 10:51:52 -0400

    using BungolProperty

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
 import asyncio
+
 from bungol import Bungol
+from bungol.models import BungolProperty
 
 
 async def main():
     """let's test something"""
     async with Bungol() as bungol:
         # resp = await bungol.search(value="117 Dawes Road")
-        kwargs = {
-            "slug": "117-dawes-road-east-york",
-            "mls_number": "E4457788",
-            "listing_id": 3837260,
-        }
-        resp = await bungol.get_property(**kwargs)
-        print(resp)
+        kwargs = {"mls_number": "E4457788", "listing_id": 3837260}
+        resp: BungolProperty = await bungol.get_property(**kwargs)
+        print(resp.info)
 
 
 if __name__ == "__main__":
